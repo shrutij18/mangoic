2c61dadb3180f83644bca8577fd5ebcd75e515d7
bca <bca@Bcalab-m24>
2023-04-06 09:10:19 +0530

    first commit

---
+++ shrutij/src/shrutij/Njo.java
@@ -0,0 +1,11 @@
+package shrutij;
+
+public class Njo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hey");
+
+	}
+
+}
---
+++ shrutij/src/shrutij/nj.java
@@ -0,0 +1,10 @@
+package shrutij;
+
+public class nj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
